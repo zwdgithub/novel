6e2ab4ef56de9b6f95d1da5fa86a887e0a50162d
zwdgithub <zwdjobs@126.com>
2019-01-23 01:42:44 +0800

    暂时去掉统一异常处理

--- src/main/java/com/company/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/company/project/configurer/WebMvcConfigurer.java
@@ -64,36 +64,36 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 	// 统一异常处理
 	@Override
 	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
-		exceptionResolvers.add(new HandlerExceptionResolver() {
-			public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response,
-					Object handler, Exception e) {
-				Result result = new Result();
-				if (e instanceof ServiceException) {// 业务失败的异常，如“账号或密码错误”
-					result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
-					logger.info(e.getMessage());
-				} else if (e instanceof NoHandlerFoundException) {
-					result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
-				} else if (e instanceof ServletException) {
-					result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
-				} else {
-					result.setCode(ResultCode.INTERNAL_SERVER_ERROR)
-							.setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
-					String message;
-					if (handler instanceof HandlerMethod) {
-						HandlerMethod handlerMethod = (HandlerMethod) handler;
-						message = String.format("接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s", request.getRequestURI(),
-								handlerMethod.getBean().getClass().getName(), handlerMethod.getMethod().getName(),
-								e.getMessage());
-					} else {
-						message = e.getMessage();
-					}
-					logger.error(message, e);
-				}
-				responseResult(response, result);
-				return new ModelAndView();
-			}
-
-		});
+//		exceptionResolvers.add(new HandlerExceptionResolver() {
+//			public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response,
+//					Object handler, Exception e) {
+//				Result result = new Result();
+//				if (e instanceof ServiceException) {// 业务失败的异常，如“账号或密码错误”
+//					result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+//					logger.info(e.getMessage());
+//				} else if (e instanceof NoHandlerFoundException) {
+//					result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
+//				} else if (e instanceof ServletException) {
+//					result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+//				} else {
+//					result.setCode(ResultCode.INTERNAL_SERVER_ERROR)
+//							.setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
+//					String message;
+//					if (handler instanceof HandlerMethod) {
+//						HandlerMethod handlerMethod = (HandlerMethod) handler;
+//						message = String.format("接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s", request.getRequestURI(),
+//								handlerMethod.getBean().getClass().getName(), handlerMethod.getMethod().getName(),
+//								e.getMessage());
+//					} else {
+//						message = e.getMessage();
+//					}
+//					logger.error(message, e);
+//				}
+//				responseResult(response, result);
+//				return new ModelAndView();
+//			}
+//
+//		});
 	}
 
 	// 解决跨域问题
